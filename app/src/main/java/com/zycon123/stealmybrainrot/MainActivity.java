4071448347a3dfae6dee5ed3fa3131d056509101
zycon123 <stian-t-95@hotmail.com>
2026-08-20 23:51:36 +0200

    Move app/src/ directory structure up one level from Steal-my-brainrot/app/src/ to app/src/

---
+++ app/src/main/java/com/zycon123/stealmybrainrot/MainActivity.java
@@ -0,0 +1,51 @@
+package com.zycon123.stealmybrainrot;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        settings.setAllowFileAccess(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        webView.evaluateJavascript(
+                "(window.Game && Game.closeTopModal && Game.closeTopModal()) ? 'closed' : 'none';",
+                value -> {
+                    if ("\"none\"".equals(value)) {
+                        if (webView.canGoBack()) webView.goBack();
+                        else MainActivity.super.onBackPressed();
+                    }
+                }
+        );
+    }
+}
